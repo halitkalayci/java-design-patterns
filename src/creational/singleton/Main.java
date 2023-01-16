a572cef0f9129505603e7b21fd427ebacd077c2f
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-01-16 11:36:13 +0300

    singleton pattern with multi-thread

--- src/creational/singleton/Main.java
+++ src/creational/singleton/Main.java
@@ -6,18 +6,42 @@ import creational.singleton.loggers.Logger;
 public class Main {
     public static void main(String[] args) {
         // Singleton pattern uygulanmadığında..
-        ILogger logger = new Logger("Database");
+       /* ILogger logger = new Logger("Database");
         ILogger logger2 = new Logger("Console");
         logger.log("No Singleton Deneme");
         logger2.log("No Singleton Deneme 2");
-        //
 
-        // Singleton pattern kullanıldığında..
+
+        //Single-Thread
+        //Singleton pattern kullanıldığında..
         ILogger logger3 = Logger.getInstance("Database");
         ILogger logger4 = Logger.getInstance("Console");
         logger3.log("Singleton Deneme");
         logger4.log("Singleton Deneme 2");
-        //
-        // 11.10'da dersteyiz..
+        */
+
+        // Multi-Thread
+        // Simülasyon..
+        // 1. api isteği, 2. api isteği..
+        Thread thread1 = new Thread(new LoggerThread1());
+        Thread thread2 = new Thread(new LoggerThread2());
+        System.out.println("Eğer tipler aynı ise, singleton pattern başarıyla uygulanmıştır.. Tipler farklı ise 2 farklı instance oluşmaktadır..");
+        thread1.start();
+        thread2.start();
+    }
+
+    static class LoggerThread1 implements Runnable{
+        @Override
+        public void run() {
+            ILogger logger = Logger.getInstance("Thread 1");
+            logger.log("Merhaba");
+        }
+    }
+    static class LoggerThread2 implements Runnable{
+        @Override
+        public void run() {
+            ILogger logger = Logger.getInstance("Thread 2");
+            logger.log("Merhaba 2");
+        }
     }
 }
--- src/creational/singleton/loggers/Logger.java
+++ src/creational/singleton/loggers/Logger.java
@@ -1,10 +1,16 @@
 package creational.singleton.loggers;
 
 public class Logger implements ILogger{
-    private static Logger instance;
+    private static volatile Logger instance;
     private String type;
 
     public Logger(String type) {
+        // 1 saniyelik bir işlem...
+        try{
+            Thread.sleep(1000);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         this.type = type;
     }
 
@@ -17,10 +23,15 @@ public class Logger implements ILogger{
     // Instance var ise return edilecek..
     public static Logger getInstance(String type){
         // instance boş ise, newle
-        if(instance==null)
-        {
-            instance = new Logger(type);
+        // locklama
+        // hali hazırda bu fonksiyon bir thread tarafından çalıştırılıyor ise..
+        // lock
+        Logger logger = instance;
+        if(logger!=null) return logger;
+        synchronized(Logger.class){
+            if(instance==null)
+                instance=new Logger(type);
+            return instance;
         }
-        return instance;
     }
 }
